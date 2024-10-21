626035931694c4fc967d39e7e14287d4f92db805
giganizer <ziga.bradas@gmail.com>
2024-10-21 23:32:55 +0200

    Solution 1593

---
+++ 1593/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def split(self, s: str, hm: set) -> int:
+        if not s:
+            return len(hm)
+        
+        max_splits = 0
+        for i in range(1, len(s) + 1):
+            ss = s[:i]
+            if ss not in hm:
+                hm.add(ss)
+                max_splits = max(max_splits, self.split(s[i:], hm))
+                hm.remove(ss)
+                
+        return max_splits
+
+    def maxUniqueSplit(self, s: str) -> int:
+        return self.split(s, set())
