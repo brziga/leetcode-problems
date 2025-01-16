ec25838307b23384606cd744e9309546d575673a
giganizer <ziga.bradas@gmail.com>
2025-01-16 22:56:20 +0100

    Faster solution 2425

--- 2425/solution.py
+++ 2425/solution.py
@@ -2,16 +2,17 @@
 
 class Solution:
     def xorAllNums(self, nums1: List[int], nums2: List[int]) -> int:
+        if len(nums1) % 2 == 0 and len(nums2) % 2 == 0: return 0
         n = 32
         set_bits = [0] * n
-        for num in nums1:
-            num_bin = bin(num)
-            for i in range(-1, -len(num_bin) + 1, -1):
-                set_bits[i] += int(num_bin[i]) * len(nums2)
-        for num in nums2:
-            num_bin = bin(num)
-            for i in range(-1, -len(num_bin) + 1, -1):
-                set_bits[i] += int(num_bin[i]) * len(nums1)
-        for i in range(n):
-            set_bits[i] = str(set_bits[i] % 2)
-        return int("".join(set_bits), 2)
+        if len(nums2) % 2 == 1:
+            for num in nums1:
+                num_bin = bin(num)
+                for i in range(-1, -len(num_bin) + 1, -1):
+                    set_bits[i] ^= int(num_bin[i])
+        if len(nums1) % 2 == 1:
+            for num in nums2:
+                num_bin = bin(num)
+                for i in range(-1, -len(num_bin) + 1, -1):
+                    set_bits[i] ^= int(num_bin[i])
+        return int("".join([str(x) for x in set_bits]), 2)
